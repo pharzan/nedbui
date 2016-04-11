f6cf9c17c879201ec1d109bd5ab3cbf73bede094
pharzan <pharzan@outlook.com>
2016-04-11 12:12:32 +0300

    two way connection from socket IO created

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
 
 var dbService=require('./dbService.js').init({path:'data/test00.db'});
+var server = require('http').createServer();
+var io = require('socket.io').listen(server);
 
-dbService.load()
+
+server.listen(8000);
+dbService.load();
+io.on('connection', function(socket){
+    console.log('a user connected');
+    socket.on('command',function(data){
+	console.log('from Client',data)
+    })
+});
+
+
+setInterval(function(){    
+    io.emit('data',{a:'hello'});
+},1000);
+
